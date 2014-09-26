4b5bd7ec7dbce7bf3046b89f3933aa3383a5b989
Witold <witold.skibniewski@gmail.com>
2014-09-26 18:20:23 +0200

    remove template code

--- MatchingGame/ViewController.swift
+++ MatchingGame/ViewController.swift
@@ -16,12 +16,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         cardView.title = "A♣️"
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func toggleCard(sender: PlayingCardView) {
