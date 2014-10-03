b148a3958ef5b173228c75e7181c5eea7e120497
Witold <witold.skibniewski@gmail.com>
2014-10-03 14:47:13 +0200

    migrate UI to separate framework
    to use live views capabilities in the IB

---
+++ CardGameUI/CardGameUI.h
@@ -0,0 +1,19 @@
+//
+//  CardGameUI.h
+//  CardGameUI
+//
+//  Created by Witold Skibniewski on 03/10/14.
+//  Copyright (c) 2014 Witold Skibniewski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CardGameUI.
+FOUNDATION_EXPORT double CardGameUIVersionNumber;
+
+//! Project version string for CardGameUI.
+FOUNDATION_EXPORT const unsigned char CardGameUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardGameUI/PublicHeader.h>
+
+
