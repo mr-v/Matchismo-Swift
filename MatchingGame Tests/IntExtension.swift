dcd41534f44683aa8ff8ca5011e335c2e463ed8f
Witold <witold.skibniewski@gmail.com>
2014-10-03 12:22:39 +0200

    add difficulty bonus (for mode with more than 2 cards to match)

---
+++ MatchingGame Tests/IntExtension.swift
@@ -0,0 +1,9 @@
+//
+//  IntExtension.swift
+//  MatchingGame
+//
+//  Created by Witold Skibniewski on 03/10/14.
+//  Copyright (c) 2014 Witold Skibniewski. All rights reserved.
+//
+
+import Foundation
